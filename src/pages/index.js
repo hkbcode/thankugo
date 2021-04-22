8ced10090edaf2c107bf3c208606c0122bca27aa
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2021-04-22 23:13:04 +0530

    added chakraUI :fire:

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const indexPage = () =>{
+	return(
+			<div style={{textAlign:"center"}}>
+				<h2> Build some Badass websites Faster</h2>
+			</div>
+
+		)
+}
+
+
+
+export default indexPage
